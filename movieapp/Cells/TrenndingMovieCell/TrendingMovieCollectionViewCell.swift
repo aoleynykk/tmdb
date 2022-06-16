3b424c1d031e252eb5e14d3841e1946404d25cd1
aoleynykk <olliynukk@gmail.com>
2022-06-16 18:47:26 +0300

    no message

---
+++ movieapp/Cells/TrenndingMovieCell/TrendingMovieCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TrendingMovieCollectionViewCell.swift
+//  movieapp
+//
+//  Created by Олександр Олійник on 16.06.2022.
+//
+
+import UIKit
+
+class TrendingMovieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var trendingMovieImage: UIImageView!
+    
+    @IBOutlet weak var trendingMovieTitle: UILabel!
+    
+    @IBOutlet weak var trendingMovieRating: UILabel!
+    
+   
+
+}
--- movieapp/Screens/Main Screen/MainViewController.swift
+++ movieapp/Screens/Main Screen/MainViewController.swift
@@ -10,53 +10,41 @@ import Alamofire
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
     
-    var trendingMoviesArr: [TrendingMovie] = []
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestTrendingMovies()
-        configureCollectionView()
-      
+       // configureCollectionView()
+       
     }
     
-    
-    private func requestTrendingMovies() {
-        AF.request(Constants().url).responseJSON { result in
-            let jsonDecoder = JSONDecoder()
-            if let responseModel = try? jsonDecoder.decode(Result.self, from: result.data!) {
-                self.trendingMoviesArr = responseModel.results!
-                self.collectionView.reloadData()
-            }
-        }
-    }
     private func configureCollectionView(){
         
-        collectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        self.collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+//        collectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+//        self.collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+        
 //        self.collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
 //        self.collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
 //        self.collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
 }
-extension MainViewController: UICollectionViewDataSource , UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+extension MainViewController: UICollectionViewDataSource , UICollectionViewDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return trendingMoviesArr.count
+    func collectionView(_ trendingMoviesCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as? MainCollectionViewCell {
-            cell.layer.cornerRadius = 10
-            cell.setup(data: trendingMoviesArr[indexPath.row])
-            return cell
-        }
-        return UICollectionViewCell()
+    func collectionView(_ trendingMoviesCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        cell.layer.cornerRadius = 10
+        
+        return cell
+        
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: collectionView.frame.width / 2.5, height: collectionView.frame.width / 2)
-    }
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        CGSize(width: collectionView.frame.width / 2.5, height: collectionView.frame.width / 2)
+//    }
 }
